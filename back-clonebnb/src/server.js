e5db7d5c8a10b6de615888f4c9c8e18a0c028f4d
Fernando Rodriguez <frodriguezv06@gmail.com>
2023-10-11 10:11:05 -0500

    config eslint and prettier

---
+++ back-clonebnb/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running in port: ${port}`);
+});
